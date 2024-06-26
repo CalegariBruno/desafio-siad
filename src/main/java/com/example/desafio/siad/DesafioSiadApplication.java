ae3b2e198da7b6a62328d535b602cd846d870b9b
bruno <brunocalegari7@gmail.com>
2024-06-26 09:31:13 -0300

    first commit

---
+++ src/main/java/com/example/desafio/siad/DesafioSiadApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio.siad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSiadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSiadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafio/siad/DesafioSiadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio.siad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSiadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
