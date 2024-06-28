71b6026ad7080def1148b82cbe875c1f9bdddad0
bruno <brunocalegari7@gmail.com>
2024-06-27 21:52:46 -0300

    update controller pessoa fisica

--- src/main/java/com/example/desafio/siad/controllers/PessoaFisicaController.java
+++ src/main/java/com/example/desafio/siad/controllers/PessoaFisicaController.java
@@ -3,37 +3,39 @@ package com.example.desafio.siad.controllers;
 import com.example.desafio.siad.domain.pessoa.Pessoa;
 import com.example.desafio.siad.domain.pessoa.PessoaFisica;
 import com.example.desafio.siad.dtos.PessoaFisicaDTO;
-import com.example.desafio.siad.services.PessoaFisicaService;
+import com.example.desafio.siad.repositories.pessoa.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("fisica")
 public class PessoaFisicaController {
 
     @Autowired
-    private PessoaFisicaService pessoaService;
+    private PessoaRepository repository;
 
     @PostMapping
-    public ResponseEntity<Pessoa> createPessoaFisica(@RequestBody PessoaFisicaDTO pessoaFisicaDTO){
-        Pessoa pessoa = pessoaService.createPessoa(pessoaFisicaDTO);
-        return new ResponseEntity<>(pessoa, HttpStatus.CREATED);
+    public ResponseEntity<Pessoa> createPessoa(@RequestBody PessoaFisicaDTO pessoaDTO){
+        Pessoa newPessoa = new PessoaFisica(pessoaDTO);
+        repository.save(newPessoa);
+        return new ResponseEntity<>(newPessoa, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<Pessoa>> getAllPessoaFisica(){
-        List<Pessoa> pessoas = pessoaService.getAllPessoas();
+        List<Pessoa> pessoas = repository.findAll();
         return new ResponseEntity<>(pessoas, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePessoa(@PathVariable String id) {
-        pessoaService.deletePessoa(id);
+        Pessoa pessoa = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Pessoa não encontrada"));
+        repository.delete(pessoa);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
