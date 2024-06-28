239dfe3cdf9a19ef963eda39f5ccd7a60cc07822
bruno <brunocalegari7@gmail.com>
2024-06-27 21:17:52 -0300

    update repositorio pessoa

---
+++ src/main/java/com/example/desafio/siad/repositories/pessoa/PessoaRepository.java
@@ -0,0 +1,7 @@
+package com.example.desafio.siad.repositories.pessoa;
+
+import com.example.desafio.siad.domain.pessoa.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, String> {
+}
